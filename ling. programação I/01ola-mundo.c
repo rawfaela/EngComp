c7251182bc1f73c36790d80b0664921c08784f6b
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-08-10 11:47:58 -0300

    Update 01ola-mundo.c

--- ling. programação I/01ola-mundo.c
+++ ling. programação I/01ola-mundo.c
@@ -5,12 +5,14 @@ int main() {
 
     float altura;
     int idade;
+    char nome[50] = "João";
 
     altura = 1.75;  
     idade = 25;       
                              //placeholder - formata a string
     printf("Altura definida: %.2fm\n", altura);
-    printf("Idade definida: %i anos\n", idade);
+    printf("Idade definida: %i anos\n", idade); //pode ser %i ou %d
+    printf("Nome definido: %s\n", nome);
 
     printf("Digite sua altura: ");
     scanf("%f",&altura);
