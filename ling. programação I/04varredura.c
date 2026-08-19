226c2fd4060dd47f8899d07a030b09007fe4e035
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-08-19 10:22:39 -0300

    Update 04varredura.c

--- ling. programação I/04varredura.c
+++ ling. programação I/04varredura.c
@@ -3,6 +3,7 @@
 int main()
 {
     char nome[] = "João da Silva";
+    char primNome[50];
 
     for (int i = 0; i < sizeof(nome); i++)
     {
@@ -16,6 +17,18 @@ int main()
         printf("%c",nome[i]);
     }
     printf("\n");
+
+    for (int i = 0; i< sizeof(nome); i++)
+    {
+        primNome[i] += nome[i];
+
+        if (nome[i] == ' ') //char é com aspas simples
+        {
+            break;
+        }
+    }
+    printf("Primeiro nome: %s\n",primNome);
+    
 
     char nome2[50];
     printf("Digite o nome: ");
