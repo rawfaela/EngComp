a1a85211bca3a083e47ef457a944dc476a70a7fa
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-09-14 11:33:30 -0300

    Update 17lista-duplamente-encadeada.c

--- ling. programação I/17lista-duplamente-encadeada.c
+++ ling. programação I/17lista-duplamente-encadeada.c
@@ -134,3 +134,144 @@ int main()
     MainMenu();
     return 0;
 }
+
+//! novo q ta fudido !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/* 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//! VER ARQ NO MOODLE, COPIAR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+typedef struct pessoa
+{
+    char nome[50];
+    int  idade;
+} PESSOA;
+
+typedef struct nodo //ponto de intersecção
+{
+    struct nodo *pProx; //ponteiro pro proximo nodo (é oq liga um no outro)
+    struct nodo *pPrev; //          '' nodo anterior ''
+    PESSOA      *pPessoa; 
+} NODO;
+
+NODO *pInicio = NULL;
+NODO *pFim    = NULL;
+NODO *pAtual  = NULL;
+
+//quando declara as funções antes (pra uma "saber" da existência da outra) é um protótipo de função
+PESSOA *CriaPessoa(void);
+NODO   *CriaNodo(void);
+void   CadastroPessoa(PESSOA *pPessoa);
+void   PrintPessoa(PESSOA *pPessoa);
+void   MainMenu(void);
+
+
+PESSOA *CriaPessoa(void) //(void) explicita que ela não recebe parametros, se fosse () os parametros não estão especificados
+{
+    PESSOA *pPessoa;
+    pPessoa = (PESSOA *)malloc(sizeof(PESSOA));
+    
+    return(pPessoa); //endereço da memoria onde a pessoa foi criada
+}
+
+NODO *CriaNodo(void)
+{
+    NODO *pNodo;
+    pNodo = (NODO *)malloc(sizeof(NODO));
+
+    pNodo->pProx   = NULL;
+    pNodo->pPrev   = NULL;
+    pNodo->pPessoa = NULL;
+
+    return(pNodo);
+}
+
+void CadastroPessoa(PESSOA *pPessoa)
+{
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", pPessoa->nome);
+    printf("Digite a idade: ");
+    scanf("%i",&pPessoa->idade);
+}
+
+void PrintPessoa(PESSOA *pPessoa)
+{ 
+    //ta printando so o ultimo
+    printf("\n--- Pessoas Cadastradas ---\n");
+    pAtual = pInicio; 
+    if (pAtual == NULL)
+    {
+        printf("Nenhuma pessoa cadastrada.");
+    }
+    while(pAtual != NULL)
+    {
+        printf("Nome: %s\n",pPessoa->nome);
+        printf("Idade: %i\n",pPessoa->idade);
+        pAtual = pAtual->pProx;
+    }
+}
+
+void AdicionaPessoa(PESSOA *pPessoa) //usou inserenalista
+{
+    NODO *pNodo;
+    pNodo          = CriaNodo();
+    pNodo->pPessoa = pPessoa;
+
+    if(pInicio == NULL)
+    {
+        pInicio = pNodo;
+        pFim    = pNodo;
+        pAtual  = pNodo;
+    }
+    else
+    {
+        pNodo->pPrev = pFim;
+        pFim->pProx  = pNodo;
+        pFim         = pNodo;
+        pAtual       = pNodo;
+    }
+}
+
+void MainMenu(void)
+{
+    int op;
+    PESSOA *pPessoa = NULL;
+
+    while (op != 3)
+    {
+        printf("\n==== MENU ====\n");
+        printf("1- Cadastrar pessoa\n2- Imprimir pessoa\n3- Sair\nEscolha uma opção: ");
+        scanf("%i",&op);
+
+        switch (op)
+        {
+            case 1:
+                PESSOA *pPessoa = CriaPessoa();
+                CadastroPessoa(pPessoa);
+                AdicionaPessoa(pPessoa);
+            break;
+            
+            case 2: 
+                PrintPessoa(pPessoa);
+            break;
+            
+            case 3:
+                printf("Saindo.\n");
+            break;
+
+            default:
+                printf("Opção inválida.\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    MainMenu();
+    return 0;
+}
+*/
