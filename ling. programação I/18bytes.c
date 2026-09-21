b2f146ba26caf295c1d5053035e8c6463ba813ff
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-09-21 10:59:48 -0300

    Update 18bytes.c

--- ling. programação I/18bytes.c
+++ ling. programação I/18bytes.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int main()
 {
     printf("Quantidade de bytes de cada tipo de variável\n");
@@ -5,4 +7,10 @@ int main()
     printf("char: %d\n",sizeof(char));
     printf("float: %d\n",sizeof(float));
     printf("double: %d\n",sizeof(double));
+
+    printf("Quantidade de bits de cada tipo de variável\n");
+    printf("int: %d\n",sizeof(int)*8);
+    printf("char: %d\n",sizeof(char)*8);
+    printf("float: %d\n",sizeof(float)*8);
+    printf("double: %d\n",sizeof(double)*8);
 }
