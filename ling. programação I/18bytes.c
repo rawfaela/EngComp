502b4ecdeec8339aa640b1b1d70b1975a6a960cc
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-09-21 10:58:08 -0300

    Create 18bytes.c

---
+++ ling. programação I/18bytes.c
@@ -0,0 +1,8 @@
+int main()
+{
+    printf("Quantidade de bytes de cada tipo de variável\n");
+    printf("int: %d\n",sizeof(int));
+    printf("char: %d\n",sizeof(char));
+    printf("float: %d\n",sizeof(float));
+    printf("double: %d\n",sizeof(double));
+}
